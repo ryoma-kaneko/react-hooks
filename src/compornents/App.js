cc00cd40ac3a638c92ff04c31c72a83e3d3f02a7
ryoma-kaneko <nekowafu0413@gmail.com>
2020-08-23 11:23:52 +0900

    cretate-context

--- src/compornents/App.js
+++ src/compornents/App.js
@@ -3,13 +3,17 @@ import reducer from "../reducers/index"
 import "bootstrap/dist/css/bootstrap.min.css";
 import EventForm from "./EventForm"
 import Events from "./Events";
+import AppContext from "../contexts/AppContext"
+console.log(AppContext)
 const App = () => {
   const [state,dispatch] = useReducer(reducer,[])
   return (
-    <div className="container-fluid">
-      <EventForm state = {state} dispatch = {dispatch}/>
-      <Events state = {state} dispatch = {dispatch}/>
-    </div>
+    <AppContext.Provider value = {"HEllo!!"}>
+      <div className="container-fluid">
+        <EventForm state = {state} dispatch = {dispatch}/>
+        <Events state = {state} dispatch = {dispatch}/>
+      </div>
+    </AppContext.Provider>
   );
 };
 export default App;
