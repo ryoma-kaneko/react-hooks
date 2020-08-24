babdb50ae729f1084e7cb49200c5fa0c59896675
ryoma-kaneko <nekowafu0413@gmail.com>
2020-08-24 11:02:11 +0900

    create operation logs

--- src/compornents/EventForm.js
+++ src/compornents/EventForm.js
@@ -1,7 +1,13 @@
 // form部分の作成
 import React,{useState,useContext} from "react";
-import { CREATE_EVENT,DELETE_ALL_EVENTS} from "../actions/index"
-import AppContext from "../contexts/AppContext"
+import { 
+    CREATE_EVENT,
+    DELETE_ALL_EVENTS,
+    ADD_OPERATION_LOG,
+    DELETE_ALL_OPERATION_LOGS
+} from "../actions/index"
+import AppContext from "../contexts/AppContext";
+import {timeCurrentIso8601} from "../utils"
 const EventForm = () => {
     const {state,dispatch} = useContext(AppContext)
     
@@ -16,6 +22,11 @@ const EventForm = () => {
                 body
             }
         )
+        dispatch({
+            type: ADD_OPERATION_LOG,
+            description: "イベントを作成しました。",
+            operatedAt: timeCurrentIso8601()
+        });
         setTitle("")
         setBody("")
     }
@@ -25,6 +36,11 @@ const EventForm = () => {
         if (result) {
             dispatch({
                 type: DELETE_ALL_EVENTS
+            })
+            dispatch({
+                type: ADD_OPERATION_LOG,
+                description: "全てのイベントを削除しました。",
+                operatedAt: timeCurrentIso8601
             })
         }
     }
