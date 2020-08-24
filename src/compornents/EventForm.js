7cac673cb845dc8e6d9f3459b703048597fb2846
ryoma-kaneko <nekowafu0413@gmail.com>
2020-08-24 11:12:57 +0900

    create-delete-operationLogs

--- src/compornents/EventForm.js
+++ src/compornents/EventForm.js
@@ -44,29 +44,63 @@ const EventForm = () => {
             })
         }
     }
-    return(
-        <>
-            <h4>イベント作成フォーム</h4>
-                <form>
-                    <div className="form-group">
-                        <label htmlFor="formEventTitle">タイトル</label>
-                        <input className="form-control" id="formEventTitle" value={title} onChange={e => setTitle(e.target.value)} />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="formEventBody">ボディー</label>
-                        <textarea className="form-control" id="formEventBody" value={body} onChange={e => setBody(e.target.value)} />
-                    </div>
-                    {/* titleまたは、bodyの入力がなかった場合は、イベントを作成しない様にdisabledで制御する */}
-                    <button className="btn btn-primary" onClick={addEvent} disabled={title === "" || body === ""}>
-                        イベントを作成する
-                    </button>
-                    {/* イベントの配列（state.length）がからの場合は、イベントを削除できない様にdisabledで制御する */}
-                    <button className="btn btn-danger" onClick={deleteAllevents} disabled={state.events.length === 0}>
-                        全てのイベントを削除する
-                    </button>
-                </form>
-        </>
-    )
+    const deleteAllOperationLogs = (e) => {
+        e.preventDefault()
+        const result = window.confirm("全ての操作ログを削除してもよろしいですか")
+        if(result){
+            dispatch({
+                type: DELETE_ALL_OPERATION_LOGS
+            })
+        }
+    }
+    return (
+      <>
+        <h4>イベント作成フォーム</h4>
+        <form>
+          <div className="form-group">
+            <label htmlFor="formEventTitle">タイトル</label>
+            <input
+              className="form-control"
+              id="formEventTitle"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="formEventBody">ボディー</label>
+            <textarea
+              className="form-control"
+              id="formEventBody"
+              value={body}
+              onChange={(e) => setBody(e.target.value)}
+            />
+          </div>
+          {/* titleまたは、bodyの入力がなかった場合は、イベントを作成しない様にdisabledで制御する */}
+          <button
+            className="btn btn-primary"
+            onClick={addEvent}
+            disabled={title === "" || body === ""}
+          >
+            イベントを作成する
+          </button>
+          {/* イベントの配列（state.length）がからの場合は、イベントを削除できない様にdisabledで制御する */}
+          <button
+            className="btn btn-danger"
+            onClick={deleteAllevents}
+            disabled={state.events.length === 0}
+          >
+            全てのイベントを削除する
+          </button>
+          <button
+            className="btn btn-danger"
+            onClick={deleteAllOperationLogs}
+            disabled={state.operationLogs.length === 0}
+          >
+            全ての操作ログを削除する
+          </button>
+        </form>
+      </>
+    );
 }
 export default EventForm
 
