fc3b0102cbbaf0feaa939ec323b4e96b118c047d
ryoma-kaneko <nekowafu0413@gmail.com>
2020-08-24 11:19:55 +0900

    create operationLogs

--- src/compornents/Event.js
+++ src/compornents/Event.js
@@ -1,12 +1,22 @@
 import React,{useContext} from "react";
-import {DELETE_EVENT} from "../actions/index"
-import AppContext from "../contexts/AppContext"
+import {
+    DELETE_EVENT,
+    ADD_OPERATION_LOG
+} from "../actions/index"
+import AppContext from "../contexts/AppContext";
+import {timeCurrentIso8601} from "../utils"
 const Event = (props) => {
     const {dispatch} = useContext(AppContext);
     const handleClickDelete = (id) => {
         const result = window.confirm(`イベント(id = ${id})を本当に削除してもよろしいですか？`)
         if(result){
             dispatch({ type: DELETE_EVENT, id })
+            dispatch({
+                type: ADD_OPERATION_LOG,
+                description: `イベント(id = ${id})を削除しました。`,
+                operatedAt: timeCurrentIso8601()
+
+            })
         }
     }
     return (
